ef43323e205a768474fd844cfe7c5b7083c6e9c0
nolixul <nadia.kaye@gmail.com>
2024-01-19 14:08:26 +0000

    Add placeholder data

---
+++ frontend/lib/definitions.ts
@@ -0,0 +1,16 @@
+export type Book = {
+    book_id: number;
+    book_owner: string;
+    book_author: string;
+    book_title: string;
+    review: string;
+    cover_image: string;
+}
+
+export type Comment = {
+    comment_id: number;
+    comment_owner: string;
+    uploaded_at: Date; // this might have to be changed to string
+    comment: string;
+    requested: boolean;
+}
