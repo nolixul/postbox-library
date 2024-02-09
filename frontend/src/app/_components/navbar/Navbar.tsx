08471615745f4f3bb035cbee5aa726a0eaaf4c04
nolixul <nadia.kaye@gmail.com>
2024-02-09 10:22:24 +0000

    Change out anchor for next links

--- frontend/src/app/_components/navbar/Navbar.tsx
+++ frontend/src/app/_components/navbar/Navbar.tsx
@@ -3,27 +3,32 @@ import '../../globals.css'
 import styles from './Navbar.module.css'
 import Image from 'next/image'
 import logo from './../../../../public/logo.png'
+import Link from 'next/link'
 
 const Navbar: React.FC = () => {
   return (
     <header className={styles.navigation}>
       <div className={styles.container}>
-        <Image src={logo} alt="" width={50} height={50} />
-        <h1>Postbox Library</h1>
+        <Link href="/">
+          <Image src={logo} alt="Postbox Library" width={50} height={50} />
+        </Link>
+        <Link className={styles.link} href="/">
+          <h1>Postbox Library</h1>
+        </Link>
       </div>
       <nav className={styles.nav_links}>
-          <a>
-            <h2 className={styles.link}>Home</h2>
-            </a>
-          <a>
-            <h2 className={styles.link}>Our Bookshelf</h2>
-          </a>
-          <a>
-            <h2 className={styles.link}>Add A Book</h2>
-          </a>
-          <a>
-            <h2 className={styles.link}>About</h2>
-          </a>
+        <Link className={styles.link} href="">
+          <h2>Home</h2>
+        </Link>
+        <Link className={styles.link} href="">
+          <h2>Our Bookshelf</h2>
+        </Link>
+        <Link className={styles.link} href="">
+          <h2>Add A Book</h2>
+        </Link>
+        <Link className={styles.link} href="/about">
+          <h2>About</h2>
+        </Link>
       </nav>
     </header>
   )
