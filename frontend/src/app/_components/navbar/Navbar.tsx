74605a8be24002519e4e19202e25b62a2ceb61cc
nolixul <nadia.kaye@gmail.com>
2024-02-09 10:15:12 +0000

    Made accessibility changes to navbar

--- frontend/src/app/_components/navbar/Navbar.tsx
+++ frontend/src/app/_components/navbar/Navbar.tsx
@@ -6,34 +6,26 @@ import logo from './../../../../public/logo.png'
 
 const Navbar: React.FC = () => {
   return (
-    <div className={styles.navigation}>
+    <header className={styles.navigation}>
       <div className={styles.container}>
         <Image src={logo} alt="" width={50} height={50} />
         <h1>Postbox Library</h1>
       </div>
-      <ul className={styles.nav_links}>
-        <li>
+      <nav className={styles.nav_links}>
           <a>
             <h2 className={styles.link}>Home</h2>
-          </a>
-        </li>
-        <li>
+            </a>
           <a>
             <h2 className={styles.link}>Our Bookshelf</h2>
           </a>
-        </li>
-        <li>
           <a>
             <h2 className={styles.link}>Add A Book</h2>
           </a>
-        </li>
-        <li>
           <a>
             <h2 className={styles.link}>About</h2>
           </a>
-        </li>
-      </ul>
-    </div>
+      </nav>
+    </header>
   )
 }
 // can't get the image to show up
