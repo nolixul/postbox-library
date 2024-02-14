5dba58d80fc7af51dae63cdb8a5bf84377e41fcc
Jess <jessicalaurenwilson1@gmail.com>
2024-02-14 17:32:39 +0000

    Remove class prop

--- frontend/src/app/Container.tsx
+++ frontend/src/app/Container.tsx
@@ -3,13 +3,10 @@ import styles from './container.module.css'
 
 interface ContainerProps {
     children: React.ReactNode
-    className?: string
 }
 
-const Container = ({ children, className }: ContainerProps) => {
-    return (
-        <article className={`${styles.main} ${className}`}>{children}</article>
-    )
+const Container = ({ children }: ContainerProps) => {
+    return <article className={`${styles.main}`}>{children}</article>
 }
 
 export default Container
