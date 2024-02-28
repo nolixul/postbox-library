14bd38375b0f27d332673ea93fbf5ab19570960f
nolixul <nadia.kaye@gmail.com>
2024-02-28 21:06:56 +0000

    Add x icon

--- frontend/src/app/_components/responsiveLinks/ResponsiveLinks.tsx
+++ frontend/src/app/_components/responsiveLinks/ResponsiveLinks.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import styles from './ResponsiveLinks.module.css'
+import Image from 'next/image'
+import xIcon from '../../../../public/x-icon.svg'
 
 interface ResponsiveLinksProps {
   isNavVisible: boolean
@@ -11,38 +13,46 @@ const ResponsiveLinks = ({ isNavVisible, toggleNav }: ResponsiveLinksProps) => {
   const path = usePathname()
 
   return (
-    <nav
-      className={`${styles.nav_links_hamburger} ${isNavVisible ? '' : styles.nav_links_hamburger_hidden}`}
-    >
-      <Link
-        onClick={toggleNav}
-        className={`${path === '/' ? styles.active_link : ''} ${styles.link}`}
-        href="/"
+    <div className={isNavVisible ? styles.container : ''}>
+      <nav
+        className={`${styles.nav_links_hamburger} ${isNavVisible ? '' : styles.nav_links_hamburger_hidden}`}
       >
-        <h1>Home</h1>
-      </Link>
-      <Link
+        <Link
+          onClick={toggleNav}
+          className={`${path === '/' ? styles.active_link : ''} ${styles.link}`}
+          href="/"
+        >
+          <h1>Home</h1>
+        </Link>
+        <Link
+          onClick={toggleNav}
+          className={`${path === '/ourbookshelf' ? styles.active_link : ''} ${styles.link}`}
+          href="/ourbookshelf"
+        >
+          <h1>Our Bookshelf</h1>
+        </Link>
+        <Link
+          onClick={toggleNav}
+          className={`${path === '/addabook' ? styles.active_link : ''} ${styles.link}`}
+          href="/addabook"
+        >
+          <h1>Add A Book</h1>
+        </Link>
+        <Link
+          onClick={toggleNav}
+          className={`${path === '/about' ? styles.active_link : ''} ${styles.link}`}
+          href="/about"
+        >
+          <h1>About</h1>
+        </Link>
+      </nav>
+      <div
+        className={isNavVisible ? '' : styles.xicon_container}
         onClick={toggleNav}
-        className={`${path === '/ourbookshelf' ? styles.active_link : ''} ${styles.link}`}
-        href="/ourbookshelf"
       >
-        <h1>Our Bookshelf</h1>
-      </Link>
-      <Link
-        onClick={toggleNav}
-        className={`${path === '/addabook' ? styles.active_link : ''} ${styles.link}`}
-        href="/addabook"
-      >
-        <h1>Add A Book</h1>
-      </Link>
-      <Link
-        onClick={toggleNav}
-        className={`${path === '/about' ? styles.active_link : ''} ${styles.link}`}
-        href="/about"
-      >
-        <h1>About</h1>
-      </Link>
-    </nav>
+        <Image src={xIcon} alt="Close navigation menu" height={20} width={20} />
+      </div>
+    </div>
   )
 }
 
