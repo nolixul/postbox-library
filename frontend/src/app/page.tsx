c9e2fecf5039fcca0762d4ec91ca626d2c9936eb
321jellyfish <98283288+321jellyfish@users.noreply.github.com>
2024-03-01 12:25:18 +0000

    add content section

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,10 +5,12 @@ export default function Home() {
   return (
     <section className={styles.container}>
     <section className={styles.container2}>
-      <h1 className={styles.title}>Welcome to Postbox Library</h1>
-      <p>Get your books here!</p>
-      <Link href="/addabook">Add a book</Link>
-      <Link href="/ourbookshelf">Our bookshelf</Link>
+      <section className={styles.content}>
+        <h1 className={styles.title}>Welcome to Postbox Library</h1>
+        <p>Get your books here!</p>
+        <Link href="/addabook">Add a book</Link>
+        <Link href="/ourbookshelf">Our bookshelf</Link>
+      </section>
     </section>
     </section>
   )
