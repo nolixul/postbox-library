fa8db25ba9de59a7290bf7be7f70eb97de3cd494
321jellyfish <98283288+321jellyfish@users.noreply.github.com>
2024-03-01 12:17:06 +0000

    set up initial HTML structure

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,13 @@
 import styles from './page.module.css'
+import Link from 'next/link'
 
 export default function Home() {
-  return <h1 className={styles.title}>Postbox Library</h1>
+  return (
+    <section className={styles.container}>
+      <h1 className={styles.title}>Welcome to Postbox Library</h1>
+      <p>Get your books here!</p>
+      <Link href="/addabook">Add a book</Link>
+      <Link href="/ourbookshelf">Our bookshelf</Link>
+    </section>
+  )
 }
