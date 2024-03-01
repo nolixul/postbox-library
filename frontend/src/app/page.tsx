106989438b9856476de5489e5ea4d89a919bcdc6
321jellyfish <98283288+321jellyfish@users.noreply.github.com>
2024-03-01 12:20:41 +0000

    set up flex container

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,10 +4,12 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <section className={styles.container}>
+    <section className={styles.container2}>
       <h1 className={styles.title}>Welcome to Postbox Library</h1>
       <p>Get your books here!</p>
       <Link href="/addabook">Add a book</Link>
       <Link href="/ourbookshelf">Our bookshelf</Link>
+    </section>
     </section>
   )
 }
