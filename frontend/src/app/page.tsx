265beca1904da5c73145a782618e55089946136f
321jellyfish <98283288+321jellyfish@users.noreply.github.com>
2024-03-08 09:52:42 +0000

    add hero image and introduction text

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,7 @@
 import styles from './page.module.css'
 import Link from 'next/link'
+import Image from 'next/image'
+import peopleReading from './_homePageImages/people_reading.jpg'
 
 export default function Home() {
   return (
@@ -7,10 +9,20 @@ export default function Home() {
     <section className={styles.container2}>
       <section className={styles.content}>
         <h1 className={styles.title}>Welcome to Postbox Library</h1>
-        <p>Get your books here!</p>
+        <p>A place to share books with friends.</p>
+        <p>If you've got a book you want to share - add a book to the bookshelf.</p>
+        <p>If you're looking for a book to read, browse the bookshelf and request a book.</p>
+        <p>Let's share the love of reading!</p>
         <Link href="/addabook">Add a book</Link>
         <Link href="/ourbookshelf">Our bookshelf</Link>
       </section>
+      <div>
+        <Image
+          src={peopleReading}
+          alt=""
+          height={400}
+          />
+      </div>
     </section>
     </section>
   )
